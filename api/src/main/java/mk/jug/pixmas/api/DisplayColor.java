8d0c2e80d6bb8b6eea21ee5e92471ee80be77081
Jordan Palikrushev <jordan.palikrushev@netcetera.com>
2015-03-14 18:24:32 +0100

    True Colors

--- api/src/main/java/mk/jug/pixmas/api/DisplayColor.java
+++ api/src/main/java/mk/jug/pixmas/api/DisplayColor.java
@@ -1,11 +1,17 @@
 package mk.jug.pixmas.api;
 
+import java.util.Arrays;
+
 public class DisplayColor {
     
     private int[] colors;
-    
+       
     public DisplayColor(int channels) {
         colors = new int[channels];
+    }
+    
+    public DisplayColor(int ... colors) {
+        this.colors = Arrays.copyOf(colors, colors.length);
     }
 
     public int[] getColors() {
