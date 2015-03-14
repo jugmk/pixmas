2d332948103416332c7a2b151f83fc23349842f4
tdelev <tomce.delev@gmail.com>
2015-03-14 19:21:50 +0100

    API code style standard change

--- api/src/main/java/mk/jug/pixmas/api/DisplayCapabilities.java
+++ api/src/main/java/mk/jug/pixmas/api/DisplayCapabilities.java
@@ -5,49 +5,49 @@ package mk.jug.pixmas.api;
  */
 public class DisplayCapabilities {
 
-    private int width;
+  private int width;
 
-    private int height;
+  private int height;
 
-    private float ppi;
+  private float ppi;
 
-    private float speed;
+  private float speed;
 
-    private int colorChannels;
+  private int colorChannels;
 
-    private int colorDepth;
+  private int colorDepth;
 
-    public DisplayCapabilities(int width, int height, float ppi, float speed, int colorChannels, int colorDepth) {
-        this.width = width;
-        this.height = height;
-        this.ppi = ppi;
-        this.speed = speed;
-        this.colorChannels = colorChannels;
-        this.colorDepth = colorDepth;
-    }
+  public DisplayCapabilities(int width, int height, float ppi, float speed, int colorChannels, int colorDepth) {
+    this.width = width;
+    this.height = height;
+    this.ppi = ppi;
+    this.speed = speed;
+    this.colorChannels = colorChannels;
+    this.colorDepth = colorDepth;
+  }
 
-    public int getWidth() {
-        return width;
-    }
+  public int getWidth() {
+    return width;
+  }
 
-    public int getHeight() {
-        return height;
-    }
+  public int getHeight() {
+    return height;
+  }
 
-    public float getPpi() {
-        return ppi;
-    }
+  public float getPpi() {
+    return ppi;
+  }
 
-    public float getSpeed() {
-        return speed;
-    }
+  public float getSpeed() {
+    return speed;
+  }
 
-    public int getColorChannels() {
-        return colorChannels;
-    }
+  public int getColorChannels() {
+    return colorChannels;
+  }
 
-    public int getColorDepth() {
-        return colorDepth;
-    }
+  public int getColorDepth() {
+    return colorDepth;
+  }
 
 }
--- api/src/main/java/mk/jug/pixmas/api/DisplayColor.java
+++ api/src/main/java/mk/jug/pixmas/api/DisplayColor.java
@@ -3,26 +3,26 @@ package mk.jug.pixmas.api;
 import java.util.Arrays;
 
 public class DisplayColor {
-    
-    private double[] colors;
-       
-    public DisplayColor(int channels) {
-        colors = new double[channels];
-    }
-    
-    public DisplayColor(double... colors) {
-        this.colors = Arrays.copyOf(colors, colors.length);
-    }
 
-    public double[] getColors() {
-        return colors;
-    }
+  private double[] colors;
 
-    public void setColors(double[] colors) {
-        this.colors = colors;
-    }
-    
-    public void setChannelColor(int channel, double color) {
-        colors[channel] = color;
-    }
+  public DisplayColor(int channels) {
+    colors = new double[channels];
+  }
+
+  public DisplayColor(double... colors) {
+    this.colors = Arrays.copyOf(colors, colors.length);
+  }
+
+  public double[] getColors() {
+    return colors;
+  }
+
+  public void setColors(double[] colors) {
+    this.colors = colors;
+  }
+
+  public void setChannelColor(int channel, double color) {
+    colors[channel] = color;
+  }
 }
